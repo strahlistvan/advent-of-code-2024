8f479c518b3e43aebdbdc2f7e05da48b837928da
Stráhl István <strahlistvan0@gmail.com>
2024-12-02 13:05:14 +0100

    bug fix

--- day01.py
+++ day01.py
@@ -1,37 +1,28 @@
-def is_ordered(report, reverse=False):
-    sorted_report = report.copy()
-    sorted_report = sorted(sorted_report, reverse=reverse)
-    return (report == sorted_report)
+from collections import Counter
 
-def is_ordered_impl(report, reverse=False):
-    for i in range(len(report)-1):
-        if report[i] <= report[i+1] and reverse:
-            return False
-        if report[i] >= report[i+1] and not reverse:
-            return False
-    return True
-
-def is_diff_ok(report, step=3):
-    prev = report[0] + 1 #init
-    for curr in report:
-        if abs(curr - prev) > step or abs(curr - prev) == 0:
-            return False
-        prev = curr
-    return True
-
-def is_report_safe(report):
-    if report[0] < report[1]: #increasing
-        return 1 if is_ordered(report, reverse=False) and is_diff_ok(report, step=3) else 0
-    elif report[0] > report[1]: #decreasing 
-        return 1 if is_ordered(report, reverse=True) and is_diff_ok(report, step=3) else 0
-    else: #constant
-        return 0
-
-with open("input/day02-input.txt", "r") as file:
-    count = 0
+list1 = []
+list2 = []
+with open("input/day01-input.txt", "r") as file:
     for line in file:
-        report = line.strip().split()
-        report = [int(s) for s in report]
-        count += is_report_safe(report)
-        print(report)
-    print(f"Biztonságos riportok száma: {count}")
+        data = line.strip().split()
+        list1.append(int(data[0]))
+        list2.append(int(data[1]))
+
+if len(list1) != len(list2):
+    print("Hiba történt")
+    exit
+
+# part 1
+
+list1.sort()
+list2.sort()
+diffs = [ abs(list2[i] - list1[i]) for i in range(len(list1)) ]
+
+print(f"A különbségek összege: {sum(diffs)}")
+
+# part 2
+
+appereances = Counter(list2)
+similarity_score = [ n * appereances.get(n, 0) for n in list1 ]
+
+print(f"A hasonlósági pontszámok összege: {sum(similarity_score)}")
