dadfd0c54fa983a603898785eaf7c6bed321becf
Stráhl István <strahlistvan0@gmail.com>
2024-12-03 17:42:23 +0100

    Day 3 solutions

--- day03.py
+++ day03.py
@@ -3,11 +3,20 @@ import re
 with open("input/day03-input.txt", "r") as file:
     input_lines = file.readlines()
 
-result = 0
+result1 = 0
+result2 = 0
+enabled = 1
 for input in input_lines:
-    expressions = re.findall("mul\(\d+,\d+\)", input)
+    expressions = re.findall("mul\(\d+,\d+\)|do\(\)|don\'t\(\)", input)
     for exp in expressions:
-        operands = re.findall("\d+", exp)
-        result += int(operands[0]) * int(operands[1])
+        if exp == "do()":
+            enabled = 1
+        elif exp == "don't()":
+            enabled = 0
+        elif "mul" in exp:
+            operands = re.findall("\d+", exp)
+            result1 += int(operands[0]) * int(operands[1])
+            result2 += ( int(operands[0]) * int(operands[1]) ) * enabled
 
-print(f"The result of multiplications: {result}")
+print(f"The result of multiplications: {result1}")
+print(f"The result of multiplications with do-s and don't-s: {result2}")
