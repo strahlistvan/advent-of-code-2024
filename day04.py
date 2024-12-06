2644be16f71b89305d37bedb217869dba66d1766
Stráhl István <strahlistvan0@gmail.com>
2024-12-06 09:49:16 +0100

    Day 4 part 1 solution

--- day04.py
+++ day04.py
@@ -1,11 +1,9 @@
-import numpy as np
-import re
-
 with open("input/day04-input.txt", "r") as file:
     letters = []
     for line in file:
         letters.append(list(line.strip()))
 
+""" # Don't need the numpy library!
 letters = np.array(letters)
 
 def count_words(letters, pattern):
@@ -22,12 +20,45 @@ def count_words_diag(letters, pattern):
         diag = "".join(letters.diagonal(i))
         count += len(re.findall(pattern, diag))
     return count
+"""
 
-xmas_count = count_words(letters, "XMAS|SAMX")
-print(xmas_count)
-xmas_count += count_words(letters.T, "XMAS|SAMX")
-print(xmas_count)
-xmas_count += count_words_diag(letters, "XMAS|SAMX")
-print(xmas_count)
-xmas_count += count_words_diag(letters.T, "XMAS|SAMX")
-print(xmas_count)
+def check_horizontal(table, row, col):
+    str = ""
+    end = min(col + 4, len(table[row]))
+    for k in range(col, end):
+        str += table[row][k]
+    return 1 if (str == "XMAS" or str == "SAMX") else 0
+
+def check_vertical(table, row, col):
+    str = ""
+    end = min(row + 4, len(table))
+    for k in range(row, end):
+        str += table[k][col]
+    return 1 if (str == "XMAS" or str == "SAMX") else 0
+
+def check_main_diag(table, row, col):
+    str = ""
+    end_row = min(row + 4, len(table))
+    end_col = min(col + 4, len(table[row]))
+    for k in range(4):
+        if row + k < end_row and col + k < end_col:
+            str += table[row + k][col + k]
+    return 1 if (str == "XMAS" or str == "SAMX") else 0
+
+def check_other_diag(table, row, col):
+    str = ""
+    end_row = min(row + 4, len(table))
+    end_col = max(col - 4, 0)
+    for k in range(4):
+        if row + k < end_row and col - k >= end_col:
+            str += table[row + k][col - k]
+    return 1 if (str == "XMAS" or str == "SAMX") else 0
+
+count = 0
+for i in range(len(letters)):
+    for j in range(len(letters[i])):
+        count += check_horizontal(letters, i, j)
+        count += check_vertical(letters, i, j)
+        count += check_main_diag(letters, i, j)
+        count += check_other_diag(letters, i, j)
+print(f"The word XMAS count in puzzle: {count}")
