720df13c3afcbbba3131bd8bb06e3b6b1a0f0a2b
Stráhl István <strahlistvan0@gmail.com>
2024-12-06 09:48:51 +0100

    Day 5 part 2 solution

--- day05.py
+++ day05.py
@@ -6,14 +6,10 @@ with open("input/day05-input.txt", "r") as file:
             rules.append(line.strip().split('|'))
         elif line.strip() != "":
             updates.append(line.strip().split(','))
-print(rules)
-print(updates)
-
 
 def check_page(page, page_idx, update):
     for rule in rules:
         if rule[1] == page and rule[0] in update[page_idx:]:
-            print(f'Talált ilyet, hogy {rule[1]} megelőzi {rule[0]}-t')
             return False
         if rule[0] == page and rule[1] in update[:page_idx]:
             return False
@@ -23,14 +19,34 @@ def get_middle(list):
     return int(list[len(list)//2])
 
 sum_middle = 0
+bad_updates = [] # prepare for part 2
 for update in updates:
     update_ok = True
     for i in range(len(update)):
         if not check_page(update[i], i, update):
             update_ok = False
-    print(str(update) + ' is ok: ' + str(update_ok))
     if update_ok:
-        print(f"A középső: {get_middle(update)}")
         sum_middle += get_middle(update)
+    else:
+        bad_updates.append(update)
 
 print(f"Sum of the good updates middle pages: {sum_middle}")
+
+# part 2 - something like bubble sort :)
+
+def correct(update):
+    for i in range(len(update)):
+        for j in range(i+1, len(update)):
+            for rule in rules:
+                if rule[0] == update[j] and rule[1] == update[i]:
+                    update[i], update[j] = update[j], update[i]
+                    # print(f"swapped: {update[i]} - {update[j]}")
+
+sum_middle = 0
+for update in bad_updates:
+    for i in range(len(update)):
+        while not check_page(update[i], i, update):
+            correct(update)
+    sum_middle += get_middle(update)
+
+print(f"Sum of the good updates middle pages after corrections: {sum_middle}")
