ad3790f866dbaf5f176b9cca0d547a42d0ac38fd
Stráhl István <strahlistvan0@gmail.com>
2024-12-10 09:09:05 +0100

    Day 8 Part 1 solution fix

--- day08.py
+++ day08.py
@@ -1,4 +1,4 @@
-from itertools import groupby, combinations
+from itertools import combinations #,groupby
 
 """ Find antenna positions from the input """
 def find_antennas(map_table):
@@ -7,7 +7,6 @@ def find_antennas(map_table):
         for col_idx, elem in enumerate(row):
             if elem not in [".", "#"]:
                 antenna_list.append({"freq": elem, "row": row_idx, "col": col_idx} )
-    # antenna_list = sorted(antenna_list, key = lambda elem: elem["freq"]) # sort list by frequency
     return antenna_list
 
 def get_distance(pair: tuple):
@@ -29,7 +28,29 @@ def get_antinodes(pair: tuple):
     antinode2 = { "row": pair[1]["row"] + dist["diff_row"], "col": pair[1]["col"] + dist["diff_col"] }
     return antinode1, antinode2
 
-if __name__ == "__main__":
+#def to_str_a(antenna: dict):
+#    return f"{antenna['freq']},{antenna['row']}|{antenna['col']}"
+
+""" For some mystical reason I could not use itertools.groupby properly... :( """
+def groupby_impl(antenna_list, key = lambda elem: elem["freq"]):
+    antenna_list = sorted(antenna_list, key = key) # sort list by key
+    group_by_freq = list()
+
+    previous_freq = antenna_list[0]["freq"]
+    group_list = list()
+    for antenna in antenna_list:
+        if antenna["freq"] == previous_freq:
+            group_list.append( antenna )
+        else:
+            group_by_freq.append(group_list)
+            group_list = list() 
+            group_list.append( antenna )
+        previous_freq = antenna["freq"]
+    
+    group_by_freq.append(group_list)
+    return group_by_freq
+
+def main():
     with open("input/day08-input.txt", "r") as file:
         map_table= file.read().split("\n")
         map_width  = len(map_table)
@@ -37,25 +58,23 @@ if __name__ == "__main__":
         print(f"Read map successfull: width: {map_width} height: {map_height}")
 
     antennas = find_antennas(map_table)
-    print("All antenna position:")
-    print(len(antennas))
 
+    # Group antennas by signal, and find all 2-element subsets (combinations)
     all_combinations = list()
-    for freq, positions in groupby(antennas, key = lambda elem: elem["freq"]):
-        position_list = list(positions)
-        print(position_list)
-        print(f"{freq} positions: {position_list}")
-        all_combinations.extend( list(combinations(position_list, 2)) )
+    for positions in groupby_impl(antennas, key = lambda elem: elem["freq"]):
+        all_combinations.extend( list(combinations(positions, 2)) )
 
-    print(f"2 elemű kombinációk: {all_combinations}")
-    antinode_list = list()
+    antinodes = set()
     for pair in all_combinations:
         a1, a2 = get_antinodes(pair)
         if is_on_map(a1, map_width, map_height):
-            antinode_list.append(str(a1))
+            antinodes.add(str(a1))
         if is_on_map(a2, map_width, map_height):
-            antinode_list.append(str(a2))
-print("All antinodes: ")
-antinode_list.sort()
-print(antinode_list)
-print(f"Different antinodes count: {len(set(antinode_list))}")
+            antinodes.add(str(a2))
+
+    #print("All antinodes: ")
+    #print(antinodes)
+    print(f"Different antinodes count: {len(antinodes)}")
+
+if __name__ == "__main__":
+    main()
