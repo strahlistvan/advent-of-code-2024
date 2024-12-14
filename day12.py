8b2762cc43d619dd01c36b96e5360e6fa70317c1
Stráhl István <strahlistvan0@gmail.com>
2024-12-14 23:22:24 +0100

    Day 12 Part 1 solution (?)

--- day12.py
+++ day12.py
@@ -1,4 +1,4 @@
-def get_region_area_perimeter(plant, garden):
+def get_region_area_perimeter(plant, garden, row_idx=-1, col_idx=-1):
 
     # result variables
     garden_region = set()
@@ -7,52 +7,49 @@ def get_region_area_perimeter(plant, garden):
 
     def increase_perimeter(orig_perim: int, row: int, col: int):
         perim = orig_perim + 4
-        print(f"increase  {row},{col} - current: {orig_perim}")
-        if (row, col-1) in garden_region and (row-1, col) in garden_region:
-            print("inc v1")
+        if (row-1, col) in garden_region and (row-1, col+1) in garden_region and (row, col+1) in garden_region \
+        and (row+1, col+1) in garden_region and (row, col+1) in garden_region and (row+1, col) in garden_region:
+            perim -= 6 # U-shape - decrease perimeter
+        elif (row, col-1) in garden_region and (row-1, col) in garden_region:
+           # print("inc v1")
             if (row-1, col-1) in garden_region:
                 perim -= 4
             else:
                 perim -= 2
         elif (row, col+1) in garden_region and (row-1, col) in garden_region:
-            print("inc v2")
+           # print("inc v2")
             if (row-1, col+1) in garden_region:
                 perim -= 4
             else:
                 perim -= 2
         elif (row, col-1) in garden_region and (row+1, col) in garden_region:
-            print("inc v3")
+          #  print("inc v3")
             if (row+1, col-1) in garden_region:
                 perim -= 4
             else:
                 perim -= 2
         elif (row, col+1) in garden_region and (row+1, col) in garden_region:
-            print("inc v4")
+         #   print("inc v4")
             if (row+1, col+1) in garden_region:
                 perim -= 4
             else:
                 perim -= 2
-   #     elif (row, col-1) in garden_region and (row+1, col-1) in garden_region  \
-   #     or   (row, col-1) in garden_region and (row-1, col-1) in garden_region  \
-   #     or   (row, col+1) in garden_region and (row+1, col+1) in garden_region  \
-  #      or   (row, col+1) in garden_region and (row-1, col+1) in garden_region:
-  #          print("inc v4.5")
-  #          perim -= 1
         elif (row, col-1) in garden_region or (row, col+1) in garden_region:
-            print("inc v5")
+           # print("inc v5")
             perim -= 2
             if (row-1, col) in garden_region:
                 perim -= 1
             if (row+1, col) in garden_region:
                 perim -= 1
-            print("inc v6")
+           # print("inc v6")
         elif (row-1, col) in garden_region or (row+1, col) in garden_region:
-            print("inc v7")
+           # print("inc v7")
             perim -= 2
             if (row, col-1) in garden_region:
                 perim -= 1
             if (row, col+1) in garden_region:
                 perim -= 1
+        #print(f"perimeter after add  {row},{col} = {perim} - all set: {garden_region}")
         return perim
 
     """ recursive inner function """
@@ -77,11 +74,14 @@ def get_region_area_perimeter(plant, garden):
         extend_area(row, col-1, plant)
 
     # main function logic
-    for i, row in enumerate(garden):
-        for j, p in enumerate(row):
-            if p == plant:
-                start_pos = (i, j)
-                break
+    if row_idx > -1 and col_idx > -1:
+        start_pos = (row_idx, col_idx)
+    else:
+        for i, row in enumerate(garden):
+            for j, p in enumerate(row):
+                if p == plant:
+                    start_pos = (i, j)
+                    break
     if start_pos:
         # recursive function call start in here
         extend_area(start_pos[0], start_pos[1], plant)
@@ -91,22 +91,27 @@ def get_region_area_perimeter(plant, garden):
 def read_input(filename):
     with open(filename, "r") as file:
         print(filename)
-        return file.read().split("\n")
+        return file.read().strip().split("\n")
 
 def main():
-    garden = ["AAAA"
-            ,"BBCD"
-            ,"BBCC"
-            ,"EEEC"]
-    garden = read_input("input/day12-test-input.txt")
+  #  garden = ["AAAA"
+  #          ,"BBCD"
+  #          ,"BBCC"
+  #          ,"EEEC"]
+    garden = read_input("input/day12-input.txt")
 
     plants = set("".join(garden))
     total_price = 0
-    #for plant in plants:
-    plant = "E"
-    region, area, perimeter = get_region_area_perimeter(plant, garden)
-    print(f" {plant} region: {region} area: {area} perimeter: {perimeter} multiply {area * perimeter}")
-    total_price += (area * perimeter)
+    all_regions = set()
+
+    for i in range(len(garden)):
+        for j, plant in enumerate(garden[i]):
+            if (i, j) not in all_regions: # field not processed yet
+              #  print(f"- {plant} növény ({i} , {j}) nincs benne a feldolgozott listában: {all_regions}")
+                region, area, perimeter = get_region_area_perimeter(plant, garden, row_idx=i, col_idx=j)
+                all_regions = all_regions.union(set(region))
+                print(f" {plant} area: {area} perimeter: {perimeter} multiply {area * perimeter}")
+                total_price += (area * perimeter)
     print(f"Total price: {total_price}")
 
 if __name__ == "__main__":
