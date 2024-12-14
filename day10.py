be002e4b91640696adcdec126ce4d56204f1833b
Stráhl István <strahlistvan0@gmail.com>
2024-12-14 10:03:47 +0100

    Day 10 part 1-2 fixed

--- day10.py
+++ day10.py
@@ -3,21 +3,15 @@ import time
 import platform
 import os
 
-with open("input/day10-test-input-3.txt", "r") as file:
-    topographic_map = file.read().strip().split("\n")
-
-ROW_COUNT = len(topographic_map)
-COL_COUNT  = len(topographic_map[0])
-hiking_trail_set = set()
-
 def clear_console():
     if platform.system() == "Windows":
         os.system("cls")
     else:
         os.system("clear")
 
+""" Visual debugging """
 def print_map(topographic_map: list, trail: str):
-    time.sleep(1)
+    time.sleep(0.3)
     clear_console()
     trail_list = [ re.findall("\d+", str) for str in trail.split() ]
     for i, row in enumerate(topographic_map):
@@ -28,35 +22,54 @@ def print_map(topographic_map: list, trail: str):
                 print(f"\033[1;37m {char}", end="")
         print("")
 
-def search_next_height(height: int, row: int, col:int, trail=""):
-    if height > 9:
-        hiking_trail_set.add(trail)
-        return
+""" Hiking trails from a given start position """
+def search_hiking_trails(row: int, col: int, topographic_map: list):
 
-    if topographic_map[row][col] == str(height):
-        trail += f" [{row},{col}]"
-        print_map(topographic_map, trail)
+    ROW_COUNT = len(topographic_map)
+    COL_COUNT  = len(topographic_map[0])
 
-        #print(f"{height+1} keresése innen indulva {row}, {col}, eddigi út {trail}")
-        if row+1 < ROW_COUNT:
-            search_next_height(height+1, row+1, col, trail)
-        if row-1 >= 0:
-            search_next_height(height+1, row-1, col, trail)
-        if col+1 < COL_COUNT:
-            search_next_height(height+1, row, col+1, trail)
-        if col-1 >= 0:
-            search_next_height(height+1, row, col-1, trail)
+    hiking_trailhead_set = set() # only start and end positions
+    hiking_trail_set = set() # full path
+
+    """ recursive inner function """
+    def search_next_height(height: int, row: int, col:int, trail=""):
+        if height > 9:
+            hiking_trail_set.add(trail)
+            trail = trail.split()[0] + ' ' + trail.split()[-1] # only start and end position
+            hiking_trailhead_set.add(trail)
+            return
+        if topographic_map[row][col] == str(height):
+            trail += f" [{row},{col}]"
+            if row+1 < ROW_COUNT:
+                search_next_height(height+1, row+1, col, trail)
+            if row-1 >= 0:
+                search_next_height(height+1, row-1, col, trail)
+            if col+1 < COL_COUNT:
+                search_next_height(height+1, row, col+1, trail)
+            if col-1 >= 0:
+                search_next_height(height+1, row, col-1, trail)
+
+    search_next_height(0, row, col, trail="")
+
+    return hiking_trailhead_set, hiking_trail_set
 
 def main():
+
+    with open("input/day10-input.txt", "r") as file:
+        topographic_map = file.read().strip().split("\n")
+
     score_sum = 0
+    score_all_sum = 0
     for i, row in enumerate(topographic_map):
         for j, pos in enumerate(row):
             if pos == "0":
-               # hiking_trail_set = set() # new empty set
-                print(f"{i},{j} helyen hiking trail keresése")
-                search_next_height(0, i, j, trail="")
-                score_sum += len(hiking_trail_set)
-                print(f"\033[1;33m Score after ({i},{j}) trailhead: {score_sum}")
+                hth_set, ht_set = search_hiking_trails(i, j, topographic_map)
+                # print(f"\033[1;37m Score of the ({i},{j}) trailhead: {len(hth_set)}")
+                # print(f"\033[1;37m Score of the ({i},{j}) trail with full path: {len(ht_set)}")
+                score_sum += len(hth_set)
+                score_all_sum += len(ht_set)
+    print(f"\033[1;37m Score after ({i},{j}) trailhead: {score_sum}")
+    print(f"\033[1;37m Score all path after ({i},{j}) trailhead: {score_all_sum} - part 2")
 
 if __name__ == "__main__":
     main()
